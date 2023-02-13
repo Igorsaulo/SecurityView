f72974751725737fa2bb360f1bc9fa9791e4a7c7
Igor Sauru <exemplo@seuemail.com.br>
2023-02-13 15:47:15 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, View, TouchableOpacity,Text } from 'react-native';
+import * as Location from "expo-location";
+import { Camera, CameraType } from "expo-camera";
+
+const initialRegion = {
+  latitude: 49.2576508,
+  longitude: -123.2639868,
+  latitudeDelta: 100,
+  longitudeDelta: 100,
+};
 
 export default function App() {
+  const [region, setRegion] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const getCurrentPosition = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+
+    if (status !== "granted") {
+      Alert.alert("Ops!", "Permissão de acesso a localização negada.");
+    }
+
+    let {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync();
+
+    setRegion({ latitude, longitude, latitudeDelta: 100, longitudeDelta: 100 });
+    setLoading(true);
+  };
+
+  useEffect(() => {
+      getCurrentPosition();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <>
+        {loading &&(
+          <View style={styles.container}>
+            <MapView style={styles.map} 
+              region={region}>
+                <Marker coordinate={{
+                latitude:Number(region.latitude),
+                longitude:Number(region.longitude)}}
+                title="You"
+              />
+            </MapView>
+          </View>
+        )}
+        <View style={styles.camera}>
+            <Camera type={CameraType.back}>
+            </Camera>
+        </View>
+      </>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+  camera:{
+    flex:1
+  }
 });
